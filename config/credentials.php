237c1db1d4a770fea909f12eec79ae566d62e396
Axel Olsson <olsson.axel@gmail.com>
2019-01-08 19:33:59 +0100

    Initial commit

---
+++ config/credentials.php
@@ -0,0 +1,6 @@
+<?php
+
+define('DB_HOST', 'database');
+define('DB_USER', 'app');
+define('DB_PASS', 'password');
+define('DB_NAME', 'todo-app-db'); // Change this depending if you use docker or vagrant
