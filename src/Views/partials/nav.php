a7a7c5b06cf15fb7acccdb8c57346de72afae157
Ruben Salas Amner <ruben.salas.amner@chasacademy.se>
2019-01-20 11:28:28 +0100

    add forms in nav view for a search bar and the toggle all method

--- src/Views/partials/nav.php
+++ src/Views/partials/nav.php
@@ -1,22 +1,20 @@
 <header class="header">
     <h1>todos</h1>
-    <form id="create-todo" method="post" action="todos">
-      <input name="title" class="new-todo" placeholder="What needs to be done?" autofocus>
+    <form class="view" method="GET" action="/todos/search">
+        <input type="text" name="search" placeholder="Search for todos?">
+        <input type="submit" name="submit" placeholder="submit">
+    </form> 
+
+   
+    <form id="create-todo" method="post" action="/todos">
+        <input name="title" class="new-todo" placeholder="What needs to be done?" autofocus>
     </form>
 </header>
 
 <section class="main">
     <form class="view" method="POST" action="/todos/toggle-all">
-    <input name="toggle" id="toggle-all" class="toggle-all" type="checkbox" 
-    <?php foreach($todos as $todo) {
-        $todo['completed'] === "false" ? 'checked="true"' : "";
-    }  ?>
-    
-    
-    onChange="submit();"
-    
-    >
-    <label for="toggle-all">Mark all as complete</label>
+        <input name="toggle" id="toggle-all" class="toggle-all" type="checkbox" onChange="submit();">
+        <label for="toggle-all">Mark all as complete</label>
     </form>
 </section>
 
